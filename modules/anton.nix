99bcb02e34056c8b09e93bc41ff159bcf904af9e
AntonAbbac <ns.miguel@proton.me>
2026-09-10 22:14:14 -0300

    Rclone for my proton drive

--- modules/anton.nix
+++ modules/anton.nix
@@ -6,38 +6,8 @@
     ./shared/anton/desktop/hyprland/programs/rofi
     ./shared/anton/desktop/hyprland/programs/waybar
     ./shared/programming/default.nix
+    ./shared/anton/desktop/proton-drive.nix
   ];
-
-  systemd.user.services.proton-drive-sync = {
-    Unit = {
-      Description = "Sincronização automática da pasta Documents com o Proton Drive";
-      After = ["network-online.target"];
-      Wants = ["network-online.target"];
-    };
-
-    Service = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.rclone}/bin/rclone sync %h/Documents proton:Documents --fast-list --transfers 4";
-      Restart = "on-failure";
-      RestartSec = "30s";
-    };
-  };
-
-  systemd.user.timers.proton-drive-sync = {
-    Unit = {
-      Description = "Timer para sincronização do Proton Drive";
-    };
-
-    Timer = {
-      OnBootSec = "2m";
-      OnUnitActiveSec = "15m";
-      Persistent = true;
-    };
-
-    Install = {
-      WantedBy = ["timers.target"];
-    };
-  };
 
   home.stateVersion = "25.11";
 }
---
+++ modules/shared/anton/desktop/proton-drive.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  systemd.user.services.proton-drive-sync = {
+    Unit = {
+      Description = "Sincronização automática da pasta Documents com o Proton Drive";
+      After = ["network-online.target"];
+      Wants = ["network-online.target"];
+    };
+
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.rclone}/bin/rclone sync %h/Documents proton:Documents --fast-list --transfers 4";
+      Restart = "on-failure";
+      RestartSec = "30s";
+    };
+  };
+
+  systemd.user.timers.proton-drive-sync = {
+    Unit = {
+      Description = "Timer para sincronização do Proton Drive";
+    };
+
+    Timer = {
+      OnBootSec = "2m";
+      OnUnitActiveSec = "15m";
+      Persistent = true;
+    };
+
+    Install = {
+      WantedBy = ["timers.target"];
+    };
+  };
+}
